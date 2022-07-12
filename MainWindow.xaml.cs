ba5f2a9b1c93e9af43e2fa609c1052c476fa7c6c
marbel82 <31281571+marbel82@users.noreply.github.com>
2022-07-12 17:13:03 +0200

    This example not working as expected, all window objects have been remove from memory

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIElementEffectMemoryLeakRepro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateVolatileWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new VolatileWindow().Show();
+        }
+
+        private void CreateLeakWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new LeakWindow().Show();
+        }
+
+        private void GCCollect_Click(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
